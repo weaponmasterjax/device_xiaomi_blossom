3e4c5fa83f8b9d59520664d960bdee90d7f3b22d
weaponmasterjax <sd369888@outlook.com>
2023-07-28 16:59:02 +0800

    blossom: Evolution-x bringup keepevolving

---
+++ evolution_blossom.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/blossom/device.mk)
+
+# Inherit some common Evolution-X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+PRODUCT_NAME := evolution_blossom
+PRODUCT_DEVICE := blossom
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+TARGET_SUPPORTS_QUICK_TAP := true
+
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
